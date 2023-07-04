78d084e82c6426854180ff130568965397edcd57
ivan kopech <ivankopech@gmail.com>
2023-07-04 17:38:32 -0300

    muestro mensajes y widget para enviar mensajes nuevos

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,26 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/chat_messages.dart';
+import '../widgets/new_message.dart';
+
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: const Icon(Icons.exit_to_app),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
+        body: Column(
+          children: const [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ));
   }
 }
